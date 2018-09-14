2d72eaa7b8ff87ed80aae277edb33e88a4386773
Yimy Romero <Yimy.Romero@smartbear.com>
2018-09-14 16:19:51 -0400

    new method.

--- connet.java
+++ connet.java
@@ -12,3 +12,12 @@ public class Connet{
 // this should go to the review #6
 // previous created another review. another test
 // previous created a new review but it also update #6. ignored pushes to all branches test.
+
+/*This method is super important :-p Will update review #6*/
+public double upset(int badday){
+	int happines = 0;
+	happines = (badday * 0) + 10000;
+	
+	return happines;
+	
+}
